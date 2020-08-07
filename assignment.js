eec1918e8f869d5be5c88ce0ee97e743172a0e2b
faisal <onehub.cloud@gmail.com>
2020-08-06 20:19:49 -0600

    assignment.js

---
+++ assignment.js
@@ -0,0 +1,40 @@
+function feetToMile(feet){
+    var miles = feet * 0.00018939;
+    return miles;
+}
+var milesOutput = feetToMile();
+console.log("Miles =", milesOutput);
+
+function  woodCalculator(chair, table, bed){
+    var woodForChair = chair * 1;
+    var woodForTable = table * 3;
+    var woodForbed = bed * 5;
+    return [woodForChair, woodForTable, woodForbed];
+    
+}
+console.log(woodForChair, woodForTable, woodForbed);
+
+function brickCalculator(feet){
+    var totalBricks = feet * 1000;
+    var floor = [];
+    if (floor <= 10){
+        feet = floor * 15;
+    }
+    else if(floor <= 20){
+        feet = floor * 12;
+    } 
+    else if (floor > 20){
+        feet = floor * 10;
+    }
+    return brickCalculator(totalbricks)
+}
+console.log ("TotaBricks=", totalBricks)
+
+Var friends = [enam, hasan, mamun, sumon, opu];
+function tinyFriend(){
+    return tinyFriend = Math.min(freinds.length)
+}
+
+
+
+
